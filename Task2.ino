ce948409ce8f09915c8a83ebd48bbf70cd4c037f
PROFOUND <1872star@gmail.com>
2026-03-27 21:17:28 +0000

    Task files

---
+++ Task2.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  delay(1000);
+
+  digitalWrite(12, LOW);
+  delay(1000);
+
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+
+  delay(1000);
+
+  digitalWrite(12, LOW);
+  delay(1000);
+
+
+  Serial.println("HI");
+}
